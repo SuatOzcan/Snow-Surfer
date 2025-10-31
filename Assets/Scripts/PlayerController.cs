f093f282b251afdad108c427c27f9cc42d9697e0
SuatOzcan <41187131+SuatOzcan@users.noreply.github.com>
2025-10-31 13:04:48 +0300

    Implement player rotation.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,15 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     InputAction moveAction;
-
+    Rigidbody2D playerRigidbody2D;
+    [SerializeField]
+    private float torqueAmount = 1.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
+        playerRigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -17,5 +20,13 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 moveVector = moveAction.ReadValue<Vector2>();
         Debug.Log(moveVector);
+        if (moveVector.x < 0.0f)
+        {
+            playerRigidbody2D.AddTorque(torqueAmount);
+        }
+        else if (moveVector.x > 0.0f)
+        {
+            playerRigidbody2D.AddTorque(-torqueAmount);
+        }
     }
 }
